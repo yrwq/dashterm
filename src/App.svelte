bb59b9e21cc52e998c259d885c7f41d00c8d4a6b
Inhof David <yrwqid@gmail.com>
2023-12-21 01:35:38 +0100

    initial, basic things was set up: key handling, command handling

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    // holds the current command
+    let cmd
+    // holds the last used command
+    let last_cmd
+
+    function handle_command() {
+        // set last cmd
+        last_cmd = cmd
+        switch(cmd) {
+            case "help": {
+                cmd = ""
+                alert("help")
+                break
+            }
+            default:
+                cmd = ""
+                break
+        }
+    }
+
+    function handle_keys(e) {
+        // enter
+	    if(e.keyCode == 13) {
+            handle_command()
+        }
+        // up arrow
+        else if (e.keyCode == 38) {
+            cmd = last_cmd
+        }
+    }
+	
+</script>
+
+<main>
+    <input id="name" type="text" bind:value={cmd} autofocus>
+</main>
+
+<style>
+</style>
+
+<svelte:window on:keydown={handle_keys} />
